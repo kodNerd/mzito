aa2f1c8da61b3337e0e87f8eb70c5592eb149df8
kodNerd <156895918+kodNerd@users.noreply.github.com>
2024-05-11 07:47:03 -0700

    1

---
+++ components/connect.php
@@ -0,0 +1,9 @@
+<?php
+
+$db_name = 'mysql:host=sql300.infinityfree.com;dbname=if0_36522057_shop_db';
+$user_name = 'if_36522057';
+$user_password = 'f0iykF5bxdxhi';
+
+$conn = new PDO($db_name, $user_name, $user_password);
+
+?>
